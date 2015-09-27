34e2ae00ba68928946e629cf6d603c6f25bc5985
René Kooi <rene@kooi.me>
2015-09-27 22:12:46 +0200

    tunbridge: create a bunch of random farmers

--- tunbridge/src/tunbridge/Main.java
+++ tunbridge/src/tunbridge/Main.java
@@ -2,13 +2,25 @@ package tunbridge;
 
 import java.lang.InterruptedException;
 import java.lang.Thread;
+import java.util.Random;
 
 public class Main {
+    final private static int FARMERS = 10;
+
     public static void main(String[] args) {
         Bridge bridge = new Bridge();
+        Random r = new Random();
 
-        cross(new SouthBoundFarmer(bridge));
-        cross(new NorthBoundFarmer(bridge));
+        System.out.println("Running with " + FARMERS + " farmers...");
+        for (int i = 0; i < FARMERS; i++) {
+            Farmer farmer;
+            if (r.nextBoolean()) {
+                farmer = new SouthBoundFarmer(bridge);
+            } else {
+                farmer = new NorthBoundFarmer(bridge);
+            }
+            cross(farmer);
+        }
     }
 
     private static void cross(Farmer f) {
